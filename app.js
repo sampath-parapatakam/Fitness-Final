cabbffce0f4a8348a1c9ebe0367741bbd3f33a97
sampath-parapatakam <43587115+sampath-parapatakam@users.noreply.github.com>
2019-05-20 19:03:30 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var expressValidator = require('express-validator');
+const sanitizeBody = require('express-validator/filter');
+const path = require('path');
+
+var router = express.Router();
+
+var catalogController = require('./controller/catalogController.js');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine','ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({secret: "mrCollections"}));
+
+app.use(expressValidator());
+app.use('/',catalogController);
+app.use('/catalog',catalogController);
+app.use('/about',catalogController);
+app.use('/contactUs',catalogController);
+app.use('/catalog/item/:id',catalogController);
+app.use('/*',catalogController);
+
+app.listen('8080',function(req,res){
+  console.log("the server has been started!!");
+  console.log("And you are listening to port 8080");
+});
